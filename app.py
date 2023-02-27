3d9151e02ae14971f391a2570205c18fbe850fe3
Jordy Bonnet <jordy.bonnet@gmail.com>
2023-02-27 21:20:49 +0100

    /calcul autensemble

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import dash_bootstrap_components as dbc
 from dash.exceptions import PreventUpdate
 import ui
 import sqlite3
+from datetime import datetime, timedelta
+import math
 
 app = dash.Dash(
     external_stylesheets=[dbc.themes.DARKLY]
@@ -12,7 +14,7 @@ server = app.server
 
 app.layout = ui.main()
 
-DEPLOYED = True
+DEPLOYED = False
 DEPLOYED_HOME_PATH = '/home/jordyB/everybodyDJ/'
 ASSETS_PATH = 'assets/'
 DEPLOYED_ASSETS_PATH = DEPLOYED_HOME_PATH + 'assets/'
@@ -75,8 +77,83 @@ def display_value(n):
     out = ui.song_lst_chld(song_list)
     return out
 
+## Autensemble calcul modal opening
+@app.callback(
+    [
+        Output('Auten-modal', 'is_open'),
+    ],
+    [
+        Input('AutEn-calcul', 'n_clicks'),
+    ],
+)
+def display_value(n):
+    # button_id = ctx.triggered_id
+    
+    if ctx.triggered[0]['value'] == None:
+        raise PreventUpdate
+    
+    return True,
 
+## Autensemble calcul btn clicked
+@app.callback(
+    [
+        Output('Aut-fade', 'is_in'),
+        Output('row-Aut-calc', 'children')
+    ],
+    [
+        Input('Aut-calc-btn', 'n_clicks'),
+    ],
+    [
+        State('input-nameC1', 'value'),
+        State('C1-date', 'value'),
+        State('input-nameC2', 'value'),
+        State('C2-date', 'value'),
+        State('kiss-date', 'value'),
+    ]
+)
+def display_value(n, name1, date1, name2, date2, kiss_date):
+    # button_id = ctx.triggered_id
+    if not ctx.triggered:
+        raise PreventUpdate
+    
+    # dates given in str with format YYYY-MM-DD
+    date1_obj = datetime.strptime(date1, '%Y-%m-%d')
+    date2_obj = datetime.strptime(date2, '%Y-%m-%d')
+    date_kiss_obj = datetime.strptime(kiss_date, '%Y-%m-%d')
+    
+    age_at_kiss1_sec = (date_kiss_obj - date1_obj).total_seconds()
+    age_at_kiss2_sec = (date_kiss_obj - date2_obj).total_seconds()
+    age_at_kiss1_year = round(age_at_kiss1_sec / 60 / 60 / 24 / 365.25)
+    age_at_kiss2_year = round(age_at_kiss2_sec / 60 / 60 / 24 / 365.25)
 
+    Aut1 = date_kiss_obj + timedelta(seconds=round(age_at_kiss1_year) * 60*60*24*365.265)
+    Aut2 = date_kiss_obj + timedelta(seconds=round(age_at_kiss2_year) * 60*60*24*365.265)
+    Aut1_str = Aut1.strftime('%d/%m/%Y')
+    Aut2_str = Aut2.strftime('%d/%m/%Y')
+    
+    poire_en_2_diff_sec = (max(Aut1, Aut2) - min(Aut1, Aut2)).total_seconds()/2
+    Aut_commun_obj = (min(Aut1, Aut2) + timedelta(seconds=poire_en_2_diff_sec)).date()
+    Aut_commun = Aut_commun_obj.strftime('%d/%m/%Y')
+
+    now = datetime.now().date()
+    when = timedelta_to_years_days(now - Aut_commun_obj)
+
+    out_child = ui.Aut_calculated(name1, Aut1_str, name2, Aut2_str, Aut_commun, when)
+    return True, out_child
+
+def timedelta_to_years_days(delta):
+    if delta.days < 0:
+        fut_pass = 'futur'
+    else:
+        fut_pass = 'past'
+    years = math.floor(abs(delta.days / 365.25))
+    if years == 0:
+        year_or_days = 'jours'
+        val = abs(delta.days)
+    else:
+        year_or_days = 'ans'
+        val = years
+    return fut_pass, val, year_or_days 
 
 if __name__ == "__main__":
     app.run_server(debug= not DEPLOYED)
--- ui.py
+++ ui.py
@@ -2,6 +2,8 @@
 
 from dash import html, dcc
 import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
+from datetime import datetime, date
 
 def main():
     # app layout
@@ -19,7 +21,21 @@ def main():
             # className="h-35",
         ),
         dbc.Row(html.Div(children=[], id='song-chld')),
-
+        dbc.Modal(
+            [
+                dbc.ModalHeader(dbc.ModalTitle(dmc.Text(
+                    "Calculez votre Autensemble ❤️",
+                    variant="gradient",
+                    gradient={"from": "yellow", "to": "red", "deg": 45},
+                    style={"fontSize": 30},
+                ))),
+                dbc.ModalBody(Aut_modal_body()),
+            ],
+            id="Auten-modal",
+            size="xl",
+            fullscreen=True,
+            is_open=False,
+        ),
         dcc.Interval(id='songs-interval', interval=2000)
     ])
 
@@ -38,20 +54,24 @@ def image_banner():
 def menu():
     return dbc.Card(
         dbc.CardBody(
-            dbc.Row([
-                dbc.Col(
-                    dbc.Input(id="input-song", placeholder="Nom de la musique", type="text", size="lg"),
-                    style=style_menu(), xs=10, sm=10, md=4, lg=4, xl=4,
-                ),
-                dbc.Col(
-                    dbc.Button("AJOUTER🎵", id='Add-song_btn', outline=False, color="success", className="me-1", size="lg"),
-                    style=style_menu(), xs=10, sm=10, md=4, lg=4, xl=4,
-                ),
-                dbc.Col(
-                    dbc.Button("Autensemble❤️", id='AutEn-calcul', outline=False, color="warning", className="me-1", size="lg"),
-                    style=style_menu(), xs=10, sm=10, md=4, lg=4, xl=4,
-                ),
-            ],justify="center",)
+        [            
+            dbc.Row(
+                [
+                    dbc.Col(
+                        dbc.Input(id="input-song", placeholder="Nom de la musique", type="text", size="lg"),
+                        style=style_menu(), xs=10, sm=10, md=4, lg=4, xl=4,
+                    ),
+                    dbc.Col(
+                        dbc.Button("AJOUTER🎵", id='Add-song_btn', outline=False, color="success", className="me-1", size="lg"),
+                        style=style_menu(), xs=10, sm=10, md=4, lg=4, xl=4,
+                    ),
+                    dbc.Col(
+                        dbc.Button("Autensemble❤️", id='AutEn-calcul', outline=False, color="warning", className="me-1", size="lg"),
+                        style=style_menu(), xs=10, sm=10, md=4, lg=4, xl=4,
+                    ),
+                ],justify="center",
+            )
+        ]
         ),
     )
 
@@ -77,13 +97,12 @@ def song_lst_chld(song_list):
     )]
     return out
 
-
 def song_card(name, score):
     return dbc.Card(
         dbc.CardBody(
             dbc.Row(
                 [
-                    dbc.Col(html.P(name), style={'fontSize': 16},
+                    dbc.Col(html.P(name), style={'fontSize': 18},
                         xs=7, sm=7, md=7, lg=7, xl=7), # xs=12, sm=12, md=12, lg=8, xl=8),
                     dbc.Col(html.P(f'{score}', style={'fontSize': 12},),
                         xs=2, sm=2, md=2, lg=2, xl=2), # xs=6, sm=6, md=6, lg=2, xl=2),
@@ -97,8 +116,179 @@ def song_card(name, score):
         style = {'height': 60}
     )
 
+def Aut_modal_body():
+    return [
+        dbc.Row(
+            dmc.Text(
+                "L'Autensemble c'est une date. Une date personnelle mais que l'on doit à notre moitié.\n \
+                C'est la date à partir de laquelle vous avez passé autant de temps avec votre moitié que sans elle dans votre vie.",
+                variant="gradient",
+                gradient={"from": "yellow", "to": "pink", "deg": 180},
+                style={"fontSize": 22},
+            )
+        ),
         
+        # Conjoint 1
+        dmc.Space(h=30),
+        dmc.Divider(label="Conjoint 1", labelPosition="center", variant="dashed", 
+                    style={"fontSize": 16}),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dbc.Input(id="input-nameC1",
+                            placeholder="Prénom conjoint n°1", type="text", size="lg",
+                        ),
+                    ],
+                    xs=6, sm=6, md=6, lg=6, xl=6,
+                ),
+                dbc.Col(
+                    [
+                        dmc.Text(
+                            "Date de naissance:",
+                            align='right',
+                            style={"fontSize": 16},
+                        )
+                    ],
+                    xs=2, sm=2, md=2, lg=2, xl=2,
+                ),
+                dbc.Col(
+                    [
+                        dmc.DatePicker(
+                            id="C1-date",
+                            value=date(1990, 1, 1),      # datetime.now().date(),
+                            inputFormat="DD / MM / YYYY",
+                            clearable=False,
+                        ),
+                    ],
+                    xs=4, sm=4, md=4, lg=4, xl=4,
+                ),
+            ]
+        ),
+
+        # Conjoint 2
+        dmc.Space(h=30),
+        dmc.Divider(label="Conjoint 2", labelPosition="center", variant="dashed", 
+                    style={"fontSize": 16}),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dbc.Input(id="input-nameC2",
+                            placeholder="Prénom conjoint n°2", type="text", size="lg",
+                        ),
+                    ],
+                    xs=6, sm=6, md=6, lg=6, xl=6,
+                ),
+                dbc.Col(
+                    [
+                        dmc.Text(
+                            "Date de naissance:",
+                            align='right',
+                            style={"fontSize": 16},
+                        )
+                    ],
+                    xs=2, sm=2, md=2, lg=2, xl=2,
+                ),
+                dbc.Col(
+                    [
+                        dmc.DatePicker(
+                            id="C2-date",
+                            value=date(1990, 1, 1),      # datetime.now().date(),
+                            inputFormat="DD / MM / YYYY",
+                            clearable=False,
+                        ),
+                    ],
+                    xs=4, sm=4, md=4, lg=4, xl=4,
+                ),
+            ]
+        ),
+
+        # First kiss
+        dmc.Space(h=30),
+        dmc.Divider(label="Premier bisous 👄", labelPosition="center", variant="dashed", 
+            style={"fontSize": 16}),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dmc.Text(
+                            "Date de premier bisous / Mise en couple:",
+                            align='right',
+                            style={"fontSize": 20},
+                        )
+                    ],
+                    xs=6, sm=6, md=6, lg=6, xl=6,
+                ),
+                dbc.Col(
+                    [
+                        dmc.DatePicker(
+                            id="kiss-date",
+                            value=date(2005, 1, 1),      # datetime.now().date(),
+                            inputFormat="DD / MM / YYYY",
+                            clearable=False,
+                        ),
+                    ],
+                    xs=4, sm=4, md=4, lg=4, xl=4,
+                ),
+            ]
+        ),
+    
+        dmc.Space(h=30),
+        dmc.Center(
+            dmc.Button("Calculer", id='Aut-calc-btn', variant="gradient", size='xl',
+                gradient={"from": "grape", "to": "pink", "deg": 35}),
+        ),
+
+        dmc.Space(h=30),
+        dbc.Fade(
+            dbc.Row(id='row-Aut-calc', children=Aut_calculated()),
+            id="Aut-fade",
+            is_in=False,
+            # appear=False,
+            style={"transition": "opacity 2000ms ease"},
+            timeout=4000, 
+        ),
+    ]
 
 
+def Aut_calculated(name1=None, Aut1=None, name2=None, Aut2=None, Aut_commun=None, when=None):
+    # when is a tuple with the time to Aut (past or futur)
+    txt = ''
+    if when:
+        if when[0] == 'futur':
+            sera_etait = 'sera dans'
+            end = 'On le fête 😲 ? Sérieux !'
+        else:
+            sera_etait = 'était il y a'
+            end = "C'est beau le 'vrai' amour 😉 !"
+        txt = f'Votre Autensemble {sera_etait} {when[1]} {when[2]}. {end}'
+    out = [
+        dmc.Text(
+                f"L'Autensemble de {name1} est le {Aut1}",
+                variant="filled",
+                color="pink",
+                style={"fontSize": 22},
+        ),
+        dmc.Text(
+                f"L'Autensemble de {name2} est le {Aut2}",
+                variant="filled",
+                color="pink",
+                style={"fontSize": 22},
+        ),
+        dmc.Text(
+                f"Et si on coupe la poire en deux c'est le : {Aut_commun}",
+                variant="filled",
+                color="red",
+                style={"fontSize": 22},
+        ),
+        dmc.Text(
+                txt,
+                variant="filled",
+                color="orange",
+                style={"fontSize": 22},
+        ),
+    ]
+    return out
 
 
